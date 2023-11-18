2065d6bb5119f60f7ccf61618a2695a97a67d9bd
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-18 15:58:56 -0800

    Update cards.js

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 export default function handler(req,res) {
   try {
     // Read the card data from your JSON file
-    const rawData=fs.readFileSync('data/cardData.json');
+    const rawData=fs.readFileSync('./data/cardData.json');
     const cardData=JSON.parse(rawData);
 
     // Return the card data in the response
