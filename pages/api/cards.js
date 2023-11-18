39ed16d11041c8c78c545a868c1819f64a170def
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-18 15:51:30 -0800

    Update cards.js

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import cardData from 'data/cardData.json'
 export default function handler(req,res) {
-  
+  const cardData =
   res.status(200).json({cardData})
 }
