405bd902e60efd83206910cd232d93a3cdac6f07
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-19 15:12:06 -0800

    hh

--- components/CardTable.page.js
+++ components/CardTable.page.js
@@ -1,3 +1,4 @@
+'use client'
 // components/CardTable.js
 import React from 'react';
 
--- pages/api/cards.page.js
+++ pages/api/cards.page.js
@@ -1,3 +1,5 @@
+
+'use client'
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import cardData from 'data/cardData.json'
 export default function handler(req,res) {
