35155c634894bf03f7bb52eae6d07ba841adf2e5
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-18 16:01:55 -0800

    Update cards.js

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -1,16 +1,6 @@
-// pages/api/cards.js
-import fs from 'fs';
-
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cardData from 'data/cardData.json'
 export default function handler(req,res) {
-  try {
-    // Read the card data from your JSON file
-    const rawData=fs.readFileSync('./data/cardData.json');
-    const cardData=JSON.parse(rawData);
-
-    // Return the card data in the response
-    res.status(200).json({ cardData });
-  } catch(error) {
-    console.error('Error reading or parsing card data:',error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  
+  res.status(200).json({ cardData })
 }
