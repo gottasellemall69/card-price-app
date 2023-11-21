bf44205d9d7a9506f2020b6c864ab337333d7aff
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-21 01:55:13 -0800

    hh

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,4 +1,17 @@
 import '@/styles/globals.css'
+import { Html,Head,Main,NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
