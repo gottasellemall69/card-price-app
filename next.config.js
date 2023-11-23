4a07d02f01d33c8343bf80a593adc439ca6ac7ce
gottasellemall69 <gottasellemall69@gmail.com>
2023-11-22 19:53:49 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig={
+const nextConfig = {
   reactStrictMode: true,
 }
-  module.exports={
-    pageExtensions: ['page.tsx','page.ts','page.jsx','page.js'],
-  }
-module.exports = nextConfig
+
+module.exports={
+  pageExtensions: ['page.tsx','page.ts','page.jsx','page.js'],
+}
