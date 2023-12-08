ad996c33ebe5889bd2a5ab6da27d33833b796e51
gottasellemall69 <gottasellemall69@gmail.com>
2023-12-08 14:35:24 -0800

    hh

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,9 +1,11 @@
 // pages/_app.js
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import '@/styles/globals.css'
 import '@/styles/index.css'
 
+
 function App({ Component,pageProps }) {
-  return <Component {...pageProps} />
+  return <><Component {...pageProps} /><SpeedInsights /></>
+  
 }
-export default App
+export default App;
